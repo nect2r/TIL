8e6f2a20820ff668a8c369cacebdf62a80ca2d1c
nect2r <nect2r@kakao.com>
2022-03-24 23:37:10 +0900

    프로그래머스 풀이

---
+++ Programmers/Java/Level_1/문자열 내 p와 y의 개수.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+/* 
+
+    대문자와 소문자가 섞여있는 문자열 s가 주어집니다. s에 'p'의 개수와 'y'의 개수를 비교해 같으면 True, 다르면 False를 return 하는 solution를 완성하세요. 'p', 'y' 모두 하나도 없는 경우는 항상 True를 리턴합니다. 단, 개수를 비교할 때 대문자와 소문자는 구별하지 않습니다.
+
+    예를 들어 s가 "pPoooyY"면 true를 return하고 "Pyy"라면 false를 return합니다.
+
+    제한사항
+    문자열 s의 길이 : 50 이하의 자연수
+    문자열 s는 알파벳으로만 이루어져 있습니다.
+
+ */
+ 
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        
+        char[] pChar = {'p','P'};
+        char[] yChar = {'y','Y'};
+        char[] sChar = s.toCharArray();
+        
+        int pInt = 0;
+        int yInt = 0;
+        
+        for(int i = 0; i < sChar.length; i++) {
+            for(int p = 0; p < pChar.length; p++) {
+                if(sChar[i] == pChar[p]){
+                    pInt++;
+                }
+            }
+            
+            for(int y = 0; y < yChar.length; y++) {
+                if(sChar[i] == yChar[y]){
+                    yInt++;
+                }
+            }
+        }
+        
+        if(pInt == yInt || (pInt == 0 && yInt == 0)) {
+            return answer;
+        }else{
+            return false;
+        }
+    }
+}
